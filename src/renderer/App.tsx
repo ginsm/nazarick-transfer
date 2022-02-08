6881c939fc8a4a0035539a53a987853eddaab41e
ginsm <mattg.0609@gmail.com>
2022-02-07 22:12:29 -0800

    Add bulma to the project
    Going with a simple CSS framework for styling

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,43 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import './scss/App.scss';
 
 const Hello = () => {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+    <div
+      className="has-text-centered"
+      style={{ maxWidth: '600px', margin: '0 auto' }}
+    >
+      <h1 className="title">Bulma</h1>
+      <p className="subtitle">
+        Modern CSS framework based on{' '}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+          Flexbox
+        </a>
+      </p>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Input" />
+        </div>
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+
+      <div className="field">
+        <p className="control">
+          <span className="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div className="buttons is-centered">
+        <button className="button is-primary" type="button">
+          Primary
+        </button>
+        <button className="button is-link" type="button">
+          Link
+        </button>
       </div>
     </div>
   );
