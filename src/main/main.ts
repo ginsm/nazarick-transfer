619014406169cded4d9d3d9b7ef749060041e44c
ginsm <mattg.0609@gmail.com>
2022-02-11 18:21:46 -0800

    Add redux devtools via electron-devtools-installer

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import Store from 'electron-store';
 import { readdir } from 'fs/promises';
+import installExtension, { REDUX_DEVTOOLS } from 'electron-devtools-installer';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
@@ -128,6 +129,12 @@ app.on('window-all-closed', () => {
 
 app
   .whenReady()
+  .then(() => {
+    // eslint-disable-next-line promise/no-nesting
+    installExtension(REDUX_DEVTOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err));
+  })
   .then(() => {
     createWindow();
     app.on('activate', () => {
--- src/renderer/store.js
+++ src/renderer/store.js
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 const hasProperty = (obj, prop) =>
   Object.prototype.hasOwnProperty.call(obj, prop);
@@ -10,90 +11,92 @@ const setter = (set, key, value) => {
   });
 };
 
-const useStore = create((set) => ({
-  // Variable defaults
-  curseForgePath: '',
-  oldProfile: '',
-  newProfile: '',
-  browseProcessing: false,
-  transferProcessing: false,
-  firstLaunch: true,
-  files: [
-    {
-      name: 'options.txt',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'servers.dat',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'config',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'resourcepacks',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'saves',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'schematics',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'screenshots',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'shaderpacks',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'XaeroWaypoints',
-      transfer: true,
-      transferred: false,
-    },
-    {
-      name: 'XaeroWorldMap',
-      transfer: true,
-      transferred: false,
-    },
-  ],
+const useStore = create(
+  devtools((set) => ({
+    // Variable defaults
+    curseForgePath: '',
+    oldProfile: '',
+    newProfile: '',
+    browseProcessing: false,
+    transferProcessing: false,
+    firstLaunch: true,
+    files: [
+      {
+        name: 'options.txt',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'servers.dat',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'config',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'resourcepacks',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'saves',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'schematics',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'screenshots',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'shaderpacks',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'XaeroWaypoints',
+        transfer: true,
+        transferred: false,
+      },
+      {
+        name: 'XaeroWorldMap',
+        transfer: true,
+        transferred: false,
+      },
+    ],
 
-  // Overwrite with electron state
-  ...(window.store.get('state') || {}),
+    // Overwrite with electron state
+    ...(window.store.get('state') || {}),
 
-  // Setters
-  setFile: (name, obj) =>
-    set((state) => {
-      const files = state.files.map((file) => {
-        if (file.name === name)
-          return {
-            ...file,
-            ...obj,
-          };
-        return file;
-      });
+    // Setters
+    setFile: (name, obj) =>
+      set((state) => {
+        const files = state.files.map((file) => {
+          if (file.name === name)
+            return {
+              ...file,
+              ...obj,
+            };
+          return file;
+        });
 
-      return { files };
-    }),
+        return { files };
+      }),
 
-  setProfile: (key, value) => setter(set, `${key}Profile`, value),
-  setProcessing: (key, value) => setter(set, `${key}Processing`, value),
-  setCurseForgePath: (value) => setter(set, 'curseForgePath', value),
-  setFirstLaunch: (value) => setter(set, 'firstLaunch', value),
-}));
+    setProfile: (key, value) => setter(set, `${key}Profile`, value),
+    setProcessing: (key, value) => setter(set, `${key}Processing`, value),
+    setCurseForgePath: (value) => setter(set, 'curseForgePath', value),
+    setFirstLaunch: (value) => setter(set, 'firstLaunch', value),
+  }))
+);
 
 // Set electron-store upon state changes
 useStore.subscribe(async (state) => {
