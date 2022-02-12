acceec9771349c565a1a2fbf4b19c6ddf35089fa
ginsm <mattg.0609@gmail.com>
2022-02-12 00:54:48 -0800

    Create utility for custom toasts stled with bulma

---
+++ src/renderer/utility/customToast.jsx
@@ -0,0 +1,60 @@
+import toast from 'react-hot-toast';
+
+const customToast = ({
+  title,
+  message,
+  level = '',
+  light = false,
+  deleteButton = false,
+  buttonText = '',
+  buttonCallback,
+  textAligned = 'center',
+  toastOptions = {},
+}) => {
+  toast.custom(
+    (t) => (
+      <div
+        className={`${
+          t.visible ? 'animate-enter' : 'animate-leave'
+        } notification ${level ? `is-${level}` : ''} ${
+          light && 'is-light'
+        } has-text-${textAligned}`}
+        style={{
+          maxWidth: '550px',
+        }}
+      >
+        {deleteButton && (
+          <button
+            className="delete"
+            type="button"
+            onClick={() => toast.dismiss(t.id)}
+          >
+            Close Toast
+          </button>
+        )}
+        {title && <h3 className="title is-5">{title}</h3>}
+        {message}
+        {buttonText && (
+          <div className="level">
+            <div className="level-left" />
+            <div className="level-right">
+              <div className="level-item">
+                <button
+                  className="button"
+                  type="button"
+                  onClick={buttonCallback || (() => toast.dismiss(t.id))}
+                  style={{ marginTop: '0.3em' }}
+                >
+                  {buttonText}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    ),
+    { ...toastOptions }
+  );
+};
+
+export default customToast;
