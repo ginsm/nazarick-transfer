3ab2c4956df587f1b583cc4a109140d0b4eae039
ginsm <mattg.0609@gmail.com>
2022-02-12 16:26:29 -0800

    Add placeholder components

---
+++ src/renderer/components/ProfileSelector.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProfileSelector = () => {
+  return <div />;
+};
+
+export default ProfileSelector;
---
+++ src/renderer/components/TransferOptions.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TransferOptions = () => {
+  return <div>Select what to transfer</div>;
+};
+
+export default TransferOptions;
