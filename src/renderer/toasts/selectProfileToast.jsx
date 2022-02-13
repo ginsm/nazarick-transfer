6dfe761c9a0759618b8e21e2b3089235603084e4
ginsm <mattg.0609@gmail.com>
2022-02-13 00:11:06 -0800

    Revise toast text

--- src/renderer/toasts/selectProfileToast.jsx
+++ src/renderer/toasts/selectProfileToast.jsx
@@ -7,12 +7,14 @@ const selectProfileToast = () => {
     message: (
       <div className="curseforge-directory__help">
         <p style={{ marginTop: '-0.5em' }}>
-          This options expects you to select your old and new profile. It will
-          move the enabled files from the old profile to the new one.
+          This options expects you to select your old and new profile. These are
+          the two profiles that will be used during the transfer (old -&gt;
+          new).
         </p>
         <p style={{ marginTop: '1em' }}>
-          You can change which files should be transferred in the config menu.
-          To get there, simply click the gear in the top right.
+          If you would like to change which files should be transferred you can
+          do so in the config menu. To get there, click the gear in the top
+          right.
         </p>
       </div>
     ),
