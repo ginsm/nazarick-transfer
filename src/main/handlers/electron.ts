c4e4971845a333a5cfb1e38a6e7ed245a143a488
ginsm <mattg.0609@gmail.com>
2022-02-13 04:14:38 -0800

    Make validCFPath reusable in file

--- src/main/handlers/electron.ts
+++ src/main/handlers/electron.ts
@@ -10,6 +10,23 @@ interface CopyFileObj {
 }
 
 const getElectronHandlers = () => {
+  const validCFPath = async (cfPath: string) => {
+    if (await pathExists(cfPath)) {
+      const files = await readdir(cfPath);
+
+      // Ensure that they selected the proper folder
+      const expectedContents = [
+        'Downloads',
+        'Export',
+        'Install',
+        'Instances',
+      ].every((file) => files.includes(file));
+
+      if (expectedContents) return true;
+    }
+    return false;
+  };
+
   return {
     browseForCF: async (_e: IpcMainInvokeEvent, oldPath: string) => {
       const selection = await dialog.showOpenDialog({
@@ -18,17 +35,8 @@ const getElectronHandlers = () => {
 
       if (!selection.canceled) {
         const directory = selection.filePaths[0];
-        const files = await readdir(directory);
-
-        // Ensure that they selected the proper folder
-        const expectedContents = [
-          'Downloads',
-          'Export',
-          'Install',
-          'Instances',
-        ].every((file) => files.includes(file));
-
-        if (expectedContents) return [directory, ''];
+        const valid = await validCFPath(directory);
+        if (valid) return [directory, ''];
         return [oldPath, 'Oops, that seems to be the wrong directory!'];
       }
 
@@ -36,28 +44,14 @@ const getElectronHandlers = () => {
     },
 
     getInstances: async (_e: IpcMainInvokeEvent, cfPath: string) => {
-      if (await pathExists(cfPath)) {
+      if (await validCFPath(cfPath)) {
         return [await readdir(path.join(cfPath, 'Instances')), ''];
       }
       return [[], 'Please change your CurseForge Directory option, thank you!'];
     },
 
-    validCFPath: async (_e: IpcMainInvokeEvent, cfPath: string) => {
-      if (await pathExists(cfPath)) {
-        const files = await readdir(cfPath);
-
-        // Ensure that they selected the proper folder
-        const expectedContents = [
-          'Downloads',
-          'Export',
-          'Install',
-          'Instances',
-        ].every((file) => files.includes(file));
-
-        if (expectedContents) return true;
-      }
-      return false;
-    },
+    validCFPath: async (_e: IpcMainInvokeEvent, cfPath: string) =>
+      validCFPath(cfPath),
 
     /**
      * This method expects that the obj has correct variables 100% of the time.
@@ -66,7 +60,7 @@ const getElectronHandlers = () => {
      * @returns
      */
     copyFile: async (_e: IpcMainInvokeEvent, obj: CopyFileObj) => {
-      if (!obj.cfPath || !obj.name || obj.profiles.length < 2) {
+      if (!obj.cfPath || !obj.name || obj.profiles.length !== 2) {
         return [
           0,
           'Improper data has been sent to the copyFile function. Stopping transfer.',
