a887db28a12f44814c70382504f7f166b5799066
ginsm <mattg.0609@gmail.com>
2022-02-12 16:23:00 -0800

    Modify message formatting

--- src/renderer/toasts/welcomeToast.jsx
+++ src/renderer/toasts/welcomeToast.jsx
@@ -8,15 +8,19 @@ const welcomeToast = () => {
       <div>
         <p style={{ marginTop: '-0.5em' }}>
           Before you can begin transferring your configurations with ease,
-          you&#39;ll need to configure this app -- this won&#39;t take long!
+          you&#39;ll need to configure this app -- this won&#39;t take very
+          long!
         </p>
-        <p style={{ marginTop: '0.5em' }}>
-          If you need help with any configuration option, you can click on the
+        <h3 className="title is-6" style={{ margin: 0, marginTop: '1.2em' }}>
+          Hint
+        </h3>
+        <p style={{ marginTop: '0.3em' }}>
+          If you need help with any configuration option you can click on this
           icon next to the option:
         </p>
         <p
           className="is-flex is-align-content-center"
-          style={{ marginTop: '0.5em' }}
+          style={{ marginTop: '0.3em' }}
         >
           <MdInfoOutline style={{ fontSize: '1.8em' }} />
         </p>
