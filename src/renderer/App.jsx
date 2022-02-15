f5506cb0e88e33cac69abdda82300675c425b1f5
ginsm <mattg.0609@gmail.com>
2022-02-15 14:48:52 -0800

    Reset transfer UI locks on launch
    This is necessary in the case of a crash or the user
    exiting whilst transferring.

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,9 +1,16 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './scss/App.scss';
+import { useEffect } from 'react';
 import HomeRoute from './routes/Home';
 import ConfigRoute from './routes/Config';
+import transfer from './utility/transfer';
 
 export default function App() {
+  // Ensures that transferProcessing is false on launch.
+  useEffect(() => {
+    transfer.suspendTransfer();
+  }, []);
+
   return (
     <>
       <Router>
